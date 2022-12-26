7852508eaed36b3e2d3d63c0bb180bedd39a2ebe
Dineshdk555 <104187302+Dineshdk555@users.noreply.github.com>
2022-12-26 19:29:07 +0530

    valid email and duplicate items update

--- src/components/users/CreateUser.js
+++ src/components/users/CreateUser.js
@@ -1,6 +1,7 @@
-import { useMutation } from "@apollo/client";
-import { Button, Grid, TextField } from "@mui/material";
+import { useQuery, useMutation } from "@apollo/client";
+import { Alert, Button, Grid, TextField } from "@mui/material";
 import { Box } from "@mui/system";
+import { useState } from "react";
 import { CREATE_SURVEY_USER } from "../../graphql/custom/mutations";
 import { LIST_SURVEY_USERS } from "../../graphql/custom/queries";
 import withSuspense from "../../helpers/hoc/withSuspense";
@@ -13,21 +14,54 @@ const initialFormValues = {
 };
 
 const CreateUser = ({ toggle }) => {
-  const [createSurveyUser, { loading, error }] = useMutation(
+  const [createSurveyUser, { loading }] = useMutation(
     CREATE_SURVEY_USER,
     {
       refetchQueries: [{ query: LIST_SURVEY_USERS }],
     }
   );
+  const { data } = useQuery(LIST_SURVEY_USERS);
+  const [error, setError] = useState(null);
   const { values, handleInputChange } = useForm(initialFormValues);
+  const [duplicate, setDuplicate] = useState(false);
+  const [userEmailDup, setUserEmailDup] = useState("");
+
+  const isValidEmail = (e) =>  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,})+$/.test(e);
+  
+  const handleEmail = (e) => {
+    if (!isValidEmail(e.target.value)) {
+      setError('Email is invalid');
+    } else {
+      setError(null);
+    }
+    handleInputChange(e);
+    setDuplicate(false);
+  };
+
+  const UserEmail = async (uemail) => {
+    let findEntries = data?.listSurveyUsers?.items?.find(
+      (s) => s?.email === uemail
+    );
+    if (findEntries) {
+      return true;
+    }
+  };
+
   const enableButton = Boolean(values.name) && Boolean(values.email);
   const onClickCreate = async () => {
-    await createSurveyUser({ variables: { input: values } });
-    toggle();
+    let dup = await UserEmail(values.email);
+    if (dup) {
+      setDuplicate(true);
+      setUserEmailDup(`${values.email} already Exists. Give another Email `);
+    } else {
+      await createSurveyUser({ variables: { input: values } });
+      toggle();
+    }
   };
   return (
     <Box>
       <Grid container spacing={2} justifyContent="center" my={1}>
+        {duplicate ? <Alert severity="error">{userEmailDup}</Alert> : null}
         <Grid item xs={12} cm={6}>
           <TextField
             required
@@ -50,9 +84,10 @@ const CreateUser = ({ toggle }) => {
             color="secondary"
             name="email"
             fullWidth
-            onChange={handleInputChange}
+            onChange={handleEmail}
             value={values.email}
           />
+           {error ? <Alert severity="error">{error}</Alert> : null}
         </Grid>
       </Grid>
       <Box
